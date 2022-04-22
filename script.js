f21035634839ddeb8bde9fc5e9bac982304031a5
Gerardo Guerrero <gerardoguerrero29@hotmail.com>
2022-04-21 22:07:28 -0300

    agregando eventos

--- script.js
+++ script.js
@@ -5,14 +5,13 @@
 
 // declaro funciones
 
- function termosuelo() {
+function termosuelo() {
     let ancho = parseInt(prompt("ingresa el ancho de la habitacion"));
     let largo = parseInt(prompt("ingresa el largo de la habitacion"));
     let area = ancho * largo;
     let costo = area * 23.50;
     alert(`El costo de Termosuelo para tu habitacion de ${area} M2 es de U$D ${costo} + IVA`);
 }
-
 function bombaDeCalor() {
     let ancho = parseInt(prompt("ingresa el ancho de la Piscina"));
     let largo = parseInt(prompt("ingresa el largo de la Piscina"));
@@ -27,64 +26,67 @@ function bombaDeCalor() {
     alert(`El costo de Bomba de calor para tu piscina de ${volumen * 1000} Litros es de U$D ${costo} + IVA`);
 }
 
+function cotizar() {
+
+    alert("Bienvenido al cotizador de Climatizacion Sustentable! para iniciar selecciona una opcion...");
+
+    let opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
+
+    do {
+        switch (opcion) {
+            case "a":
+                termosuelo();
+                alert("Gracias por cotizar Termosuelo! te esperamos pronto!");
+                opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
+                break;
+
+            case "b":
+                bombaDeCalor();
+                alert("Gracias por cotizar Bomba de Calor! te esperamos pronto!");
+                opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
+                break;
+
+            case "x":
+                alert("Gracias por utilizar el cotizador! te esperamos pronto!");
+
+                break;
+
+            default:
+                alert("Opcion Incorrecta.")
+                opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
+
+                break;
+        }
+    } while (opcion != "x");
+}
+
+function cargarHab(hab) {
+    
+    for (let i = 0; i < 4; i++) {
+        const habitacion = prompt("ingrese 4 habitaciones");
+        hab.push(habitacion);
+        console.log(hab)
+    }
+    
+    alert(`el array ingresado es ${hab}`);
+}
+
+const borrarhab = (hab) => {
+    
+    let eliminar = prompt("ingrese que habitacion desea eliminar");
+    let i = hab.indexOf(eliminar);
+    if (i == -1) {
+        alert("Nombre incorrecto ingresado");
+    } else {
+        hab.splice(i, 1);
+        alert(`el array resultante es ${hab}`);
+    }
+}
 //inicio
 
-alert("Bienvenido al cotizador de Climatizacion Sustentable! para iniciar selecciona una opcion...");
+const cotizador = document.querySelector("#ctz");
+const habitaciones = document.querySelector("#hab");
+let hab = [];
 
-let opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-
-do {
-    switch (opcion) {
-        case "a":
-            termosuelo();
-            alert("Gracias por cotizar Termosuelo! te esperamos pronto!");
-            opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-            break;
-
-        case "b":
-            bombaDeCalor();
-            alert("Gracias por cotizar Bomba de Calor! te esperamos pronto!");
-            opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-            break;
-
-        case "x":
-            alert("Gracias por utilizar el cotizador! te esperamos pronto!");
-
-            break;
-
-        default:
-            alert("Opcion Incorrecta.")
-            opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-
-            break;
-    }
-} while (opcion !="x");
-
-
-const hab= [];
-
-for (let i = 0; i < 4; i++) {
-    const habitacion = prompt("ingrese 4 habitaciones");
-    hab.push(habitacion);
-    console.log(hab) 
-}
-
-alert(`el array ingresado es ${hab}`);
-
-
-// declaro una funcion para borrar
-
-const borrar=(eliminar)=> {
-    let i= hab.indexOf(eliminar);
-    if (i==-1){
-        alert("Nombre incorrecto ingresado");
-    }else{
-        hab.splice(i,1);
-    }
-}
-
-// utilizo la funcnion borrar
-
-let eliminar=prompt("ingrese que habitacion desea eliminar");
-borrar(eliminar);
-alert(`el array resultante es ${hab}`);
+cotizador.onclick= ()=> {cotizar()};
+habitaciones.addEventListener("click",cargarHab);
