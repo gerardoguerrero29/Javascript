376beae6c2a99932429bf35e7ea213aae6ce2f1a
Gerardo Guerrero <gerardoguerrero29@hotmail.com>
2022-05-27 16:41:46 -0300

    modificaciones previas a entrega final

--- script.js
+++ script.js
@@ -3,84 +3,6 @@
 // Mi proyecto se trata acerca de un cotizador para calcular el costo de un equipo para climatizacion de hogares o piscina 
 
 
-// declaro funciones
-
-/*
-
-
-function cotizar() {
-
-    alert("Bienvenido al cotizador de Climatizacion Sustentable! para iniciar selecciona una opcion...");
-
-    let opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-
-    do {
-        switch (opcion) {
-            case "a":
-                termosuelo();
-                alert("Gracias por cotizar Termosuelo! te esperamos pronto!");
-                opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-                break;
-
-            case "b":
-                bombaDeCalor();
-                alert("Gracias por cotizar Bomba de Calor! te esperamos pronto!");
-                opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-                break;
-
-            case "x":
-                alert("Gracias por utilizar el cotizador! te esperamos pronto!");
-
-                break;
-
-            default:
-                alert("Opcion Incorrecta.")
-                opcion = prompt("a-Termosuelo b-Bomba de Calor x-SALIR");
-
-                break;
-        }
-    } while (opcion != "x");
-}
-
-function cargarHab() {
-
-    for (let i = 0; i < 4; i++) {
-        const habitacion = prompt("ingrese 4 habitaciones");
-        hab.push(habitacion);
-        console.log(hab)
-    }
-
-    alert(`el array ingresado es ${hab}`);
-}
-
-const borrarHab = () => {
-
-    let eliminar = prompt("ingrese que habitacion desea eliminar");
-    let i = hab.indexOf(eliminar);
-    if (i == -1) {
-        alert("Nombre incorrecto ingresado");
-    } else {
-        hab.splice(i, 1);
-        alert(`el array resultante es ${hab}`);
-    }
-}
-//inicio
-
-const cotizador = document.querySelector("#ctz");
-const habitaciones = document.querySelector("#hab");
-const bohab = document.querySelector("#bohab");
-const hab = [];
-
-cotizador.onclick = () => { cotizar() };
-habitaciones.addEventListener("click", cargarHab);
-bohab.addEventListener("click", borrarHab);
-
-cotizador.addEventListener("mouseover", () => {
-    console.log("pasaste por cotizador");
-})
-
-*/
-
 
 //tomo los datos del usuario
 
@@ -88,6 +10,7 @@ const select = document.querySelector("#selectProducto");
 const ancho = document.querySelector("#ancho");
 const largo = document.querySelector("#largo");
 const btnCotizar = document.querySelector("#ctz");
+const ambientes= document.querySelector("ambientes");
 const tc = document.querySelector("#tc")
 
 let inputTipo = select.onchange = () => {
@@ -120,6 +43,7 @@ function termosuelo() {
         imageAlt: 'termosuelo',
         icon: "success",
     });
+    return costo;
 }
 function bombaDeCalor() {
     let ancho = inputAncho;
@@ -140,7 +64,7 @@ function bombaDeCalor() {
         imageAlt: 'Bomba de Calor',
         icon: "success",
     });
-
+    return costo;
 }
 
 //lanzo la cotizacion y almaceno el objeto en el array y en el local storage
@@ -148,17 +72,21 @@ function bombaDeCalor() {
 const listaProductos = JSON.parse(localStorage.getItem("productos")) || [];
 
 class Producto {
-    constructor(tipo, ancho, largo) {
+    constructor(tipo, ancho, largo, preciousd, precioars) {
         this.tipo = tipo;
         this.ancho = ancho;
         this.largo = largo;
+        this.preciousd=preciousd;
+        this.precioars=precioars;
     }
 }
 const nuevoProducto = () => {
     let tipo = inputTipo;
     let ancho = inputAncho;
     let largo = inputLargo;
-    let producto = new Producto(tipo, ancho, largo);
+    let precioars=0;
+    let preciousd=0;
+    let producto = new Producto(tipo, ancho, largo, preciousd, precioars);
     listaProductos.push(producto);
     console.log("producto almacenado en el array");
     localStorage.setItem("productos", JSON.stringify(listaProductos));
@@ -171,7 +99,7 @@ btnCotizar.onclick = (e) => {
     switch (inputTipo) {
         case "termosuelo":
             nuevoProducto();
-            termosuelo();
+            this.producto.preciousd=termosuelo()
             break;
 
         case "bombaDeCalor":
@@ -189,14 +117,15 @@ const obtenerCotizacion = () => {
         .then(response => response.json())
         .then((cotizacion) => {
             console.log(cotizacion);
-
             for (let i = 0; i < 2; i++) {
                 console.log(cotizacion[i]);
 
                 tc.innerHTML += `
-                <h3> ${cotizacion[i].nombre} </h3>
-                <p> TC compra $${cotizacion[i].compra} TC Venta $${cotizacion[i].Venta} </p>
+                <h3> ${cotizacion.nombre} </h3>
+                <p> TC compra $${cotizacion.compra} TC Venta $${cotizacion.Venta} </p>
                 `
+                console.log(cotizacion[i].nombre)
+
             }
         })
 }
